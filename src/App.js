4ac5744ab78ac3f04935785805316ae4ba499d5a
vivienrauch <vivienmirjam@gmail.com>
2023-11-26 15:29:50 +0000

    Add basic post create form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from "react-router-dom";
 import './api/axiosDefaults';
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
+import PostCreateForm from "./pages/posts/PostCreateForm";
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/bookclubevents" render={() => <h1>Book Club Events</h1>} />
           <Route exact path="/bookofthemonth" render={() => <h1>Book Of The Month</h1>} />
+          <Route exact path="/posts/create" render={() => <PostCreateForm /> } />
           <Route render={() => <p>Page Not Found</p>} />
         </Switch>
       </Container>
