1de96c37ad83fb23c09d40e643cd62480b588874
vivienrauch <vivienmirjam@gmail.com>
2024-01-18 22:28:08 +0000

    Correcting App.js imports

--- src/pages/bookclubevents/BookClubEventsPage.js
+++ src/pages/bookclubevents/BookClubEventsPage.js
@@ -6,7 +6,7 @@ import appStyles from "../../App.module.css";
 import styles from "../styles/BookClubEventsPage.module.css";
 import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
-import NoResults from "../assets/no-results.png";
+import NoResults from "../../assets/no-results.png";
 import PopularProfiles from "../profiles/PopularProfiles";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
