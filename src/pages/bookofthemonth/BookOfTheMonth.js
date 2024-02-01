3a857c9f137e22a53b38dd14cf23b6a750053312
vivienrauch <vivienmirjam@gmail.com>
2024-02-01 15:29:46 +0000

    Add styling to the book of the month component

--- src/pages/bookofthemonth/BookOfTheMonth.js
+++ src/pages/bookofthemonth/BookOfTheMonth.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { axiosReq } from "../../api/axiosDefaults";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { useLocation } from "react-router";
+import styles from "../../styles/BookOfTheMonth.module.css";
 
 
 const BookOfTheMonth = () => {
@@ -26,14 +27,15 @@ const BookOfTheMonth = () => {
   }, [pathname, currentUser]);
   
   return (
-    <div>
+    <div className={styles.Container}>
       {books && books.map((book) => (
-        <div key={book.id}>
+        <div key={book.id} className={styles.Card}>
           <p>{book.created_at}</p>
           <h2>{book.title}</h2>
           <p>{book.content}</p>
-          <img src={book.image} alt="Book cover" />
-          <p>Read more and buy: {book.website}</p>
+          <a href={book.website} target="_blank" rel="noopener noreferrer">
+            <img className={styles.CoverImage} src={book.image} alt="Book cover" />
+          </a>
         </div>
       ))}
     </div>
